53d99f557f391b659d183ca9c55a40566c7932af
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-11-30 09:36:40 +0100

    ft/register

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,16 @@
+import CreateAccount from '@/page/auth/create-account'
+import React from 'react'
+
+type Props = {}
+
+const Register = (props: Props) => {
+  return (
+      <div className="h-screen w-full grid place-items-center">
+          <div className="w-full  md:w-[40%]">
+              <CreateAccount />
+          </div>
+    </div>
+  )
+}
+
+export default Register
