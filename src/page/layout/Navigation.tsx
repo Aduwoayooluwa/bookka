e4f0d507a08312bfdb0a5f03df889b9ba5ada95d
Anita Eghonghon Idemudia <Ieghonghon@gmail.com>
2023-12-06 01:48:46 +0100

    landing page

--- src/page/layout/Navigation.tsx
+++ src/page/layout/Navigation.tsx
@@ -16,23 +16,23 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white container mx-auto">
-      <div className="w-full mx-auto px-4">
+    <nav className="bg-slate-100 mx-auto">
+      <div className="w-full mx-auto px-32">
         <div className="flex justify-between items-center">
           <div className="flex space-x-7">
             <div>
               <Link href="/">
-                <span className="flex items-center py-4 px-2 cursor-pointer font-semibold text-black text-lg">Bookka</span>
+                <span className="flex items-center py-4 px-2 cursor-pointer text-4xl font-bold text-black">Bookka</span>
               </Link>
             </div>
             </div>
             <div className="hidden md:flex items-center space-x-4">
-                <div className="hidden md:flex items-center space-x-1">
-                    <Link href="/about"><span className="py-4 px-2 text-blue-500 font-semibold cursor-pointer">About</span></Link>
-                    <Link href="/services"><span className="py-4 px-2 text-blue-500 font-semibold cursor-pointer">Services</span></Link>
-                    <Link href="/contact"><span className="py-4 px-2 text-blue-500 font-semibold cursor-pointer">Contact</span></Link>
-                    </div>
-                    <button className="rounded-full bg-blue-500 text-white py-2 px-4  hover:bg-blue-600">
+                {/* <div className="hidden md:flex items-center space-x-1">
+                    <Link href="/about"><span className="py-4 px-2 text-black uppercase cursor-pointer">About</span></Link>
+                    <Link href="/services"><span className="py-4 px-2 text-black uppercase cursor-pointer">Services</span></Link>
+                    <Link href="/contact"><span className="py-4 px-2 text-black uppercase cursor-pointer">Contact</span></Link>
+                    </div> */}
+                    <button className="rounded-sm bg-blue-500 uppercase text-white py-2 px-4  hover:bg-blue-600">
                         <Link href="/auth">Get Started</Link>
                     </button>
                   </div>
